88338b82b4aadd72d0212d7c2a046a3ecbf24e1e
Sandeep Bohra <sandeepbohra.sb@gmail.com>
2018-12-19 12:13:27 +0530

    renamed Components to components

---
+++ src/shared/components/input-places-autocomplete/InputPlacesAutocomplete.js
@@ -0,0 +1,56 @@
+import React, { Component } from 'react';
+import { googleMaps } from '../../../directions/services/google-maps/googleMap'
+import './InputPlacesAutocomplete.css'
+
+class InputPlacesAutocomplete extends Component {
+    inputAutoComplete;
+    constructor(props) {
+        super(props)
+        this.state = {
+            address: '',
+        }
+    }
+
+    componentDidMount() {
+        this.renderAutoComplete();
+    }
+
+    renderAutoComplete = async () => {
+        const maps = await this.props.googleMaps();
+        this.inputAutoComplete = new maps.places.Autocomplete(this.inputElem);
+        this.inputAutoComplete.addListener('place_changed', ()=>{
+            const place = this.inputAutoComplete.getPlace()
+            if (place) {
+                this.props.handleOnSelectAddress(place, this.props.inputName)
+            }
+            console.log(this.inputAutoComplete.getPlace())
+        })
+    };
+
+    componentDidUpdate() {
+        debugger
+        if(!this.props.location) {
+            this.inputElem.value = null
+        }
+    }
+
+
+    render() {
+        return(
+            <div className="InputPlacesAutocomplete">
+                <p>{this.props.label}</p>
+                <input 
+                    type="text" 
+                    name={this.props.inputName} 
+                    ref={elem => (this.inputElem = elem)}
+                />
+            </div>
+        )
+    }
+}
+
+export default InputPlacesAutocomplete;
+
+InputPlacesAutocomplete.defaultProps = {
+    googleMaps,
+}
