08b510f48d06ed0381b71871c1a8eea89f22084a
Sandeep Bohra <sandeepbohra.sb@gmail.com>
2018-12-19 12:29:41 +0530

    removing duplicate directory due to case insensitvity

--- src/directions/Constants/apiURL.js
+++
@@ -1,4 +0,0 @@
-export const MOCK_API_DETAILS = {
-    URL: "http://localhost:8080",
-    Route: "/route",
-}
--- src/directions/Constants/errorTypes.js
+++
@@ -1,3 +0,0 @@
-export const MOCK_API_ERROR = "Internal server error"
-export const START_LOCATION_EMPTY = "Start location can't be empty"
-export const DROP_OFF_LOCATION_EMPTY = "Drop-off location can't be empty"
