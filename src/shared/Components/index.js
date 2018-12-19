5ba815566781e687101ccbc70067965035f8aa4c
Sandeep Bohra <sandeepbohra.sb@gmail.com>
2018-12-19 12:08:39 +0530

    reverted the folder deletion

---
+++ src/shared/Components/index.js
@@ -0,0 +1,2 @@
+export {default as InputPlacesAutocomplete} from './input-places-autocomplete/InputPlacesAutocomplete'
+export {default as Loader} from './loader/Loader'
---
+++ src/shared/Components/loader/__tests__/Loader.test.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+import Loader from '../Loader'
+
+describe('<Loader /> component', () => {
+  
+  it('should match the snapshot', () => {
+    const output = renderer.create(<Loader />).toJSON();
+    expect(output).toMatchSnapshot();
+  })
+})
