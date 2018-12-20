018e03f2ff06239237e49c6473fd85ac792998c4
Sandeep Bohra <sandeepbohra.sb@gmail.com>
2018-12-20 17:48:13 +0530

    added code comments

--- src/directions/services/google-maps/googleMap.js
+++ src/directions/services/google-maps/googleMap.js
@@ -2,6 +2,7 @@ import GoogleMapsLoader from 'google-maps';
 
 const googleAPIKey = process.env.REACT_APP_GOOGLE_MAP_API_KEY
 
+// The below method checks if the google maps API key is configured in .env file
 const checkIfGoogleApiKeyAvailable = () => {
     if(!googleAPIKey) {
         alert("Google API key not found, please refer readme.md for more information!!");
@@ -10,6 +11,7 @@ const checkIfGoogleApiKeyAvailable = () => {
     }
 }
 
+// This method is used to initialize google maps with settings
 const initializeGoogleMapSettings = () => {
     if(checkIfGoogleApiKeyAvailable()) {
         GoogleMapsLoader.KEY = googleAPIKey;
@@ -20,6 +22,8 @@ const initializeGoogleMapSettings = () => {
 initializeGoogleMapSettings();
 
 let google;
+
+// The below method is loading google map library
 const loadMap = () =>
     new Promise((resolve, reject) => {
         if (google) {
